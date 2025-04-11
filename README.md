ef5f7ea24daaba44adc9df270a5e4ea4224e8681
aaquaa <113173827+aaquaa@users.noreply.github.com>
2025-04-11 21:20:51 +1000

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+# FRC Scout
+
+A QR Code Based Scouting System for FRC
+
+3132 Thunder Down Under
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        rhr: ['"Lato-Regular"'],
+        'rhr-ns': ['"Roboto-Thin"'],
+      },
+      colors: {
+        red: { rhr: '#000000' },
+      },
+    },
+  },
+  plugins: [require('@tailwindcss/forms')],
+}
